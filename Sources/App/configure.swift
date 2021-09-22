50919f3052abcd82438eaee59ccc36e4e6051620
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-09-22 23:51:45 +0200

    Added a middleware to disable FLoC

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,10 +2,24 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
+/// Middleware telling the google pervasive-tracking to fuck off
+class FLoCMiddleware: Middleware {
+	static let flocHeader = "Permissions-Policy"
+	func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+		let response = next.respond(to: request)
+		return response.map { response in
+			response.headers.replaceOrAdd(name: Self.flocHeader, value: "interest-cohort=()")
+			return response
+		}
+	}
+}
+
 // configures your application
 public func configure(_ app: Application) throws {
 	// uncomment to serve files from /Public folder
 	// app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+	app.middleware.use(FLoCMiddleware())
 
 	app.databases.use(.postgres(
 		hostname: Environment.get("DATABASE_HOST") ?? "localhost",
