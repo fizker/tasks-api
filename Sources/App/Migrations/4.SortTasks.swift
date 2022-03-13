66f9810d719b783485be8e38a599ffbd996d646e
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-03-13 12:01:46 +0100

    Rewrote the SortTasks migration to support non-postgres dbs
    - It only supports them if they have no data

--- Sources/App/Migrations/4.SortTasks.swift
+++ Sources/App/Migrations/4.SortTasks.swift
@@ -18,33 +18,38 @@ private enum MigrationError: Error {
 	case notPostgres
 }
 
-struct SortTasks: Migration {
-	func prepare(on database: Database) -> EventLoopFuture<Void> {
-		var sortOrder = 1
-		return database.transaction { (database) -> EventLoopFuture<Void> in
-			return database.schema(Task.schema)
+struct SortTasks: AsyncMigration {
+	func prepare(on database: Database) async throws {
+		return try await database.transaction { (database) async throws in
+			try await database.schema(Task.schema)
 				.field("sort_order", .int)
 				.update()
-				.flatMap { _ in database.query(SortableTask.self).all() }
-				.flatMapEach(on: database.eventLoop.next()) { task -> EventLoopFuture<Void> in
-					task.sortOrder = sortOrder
-					sortOrder += 1
-					return task.save(on: database)
-				}
-				.map { _ in database as? PostgresDatabase }
-				.unwrap(or: MigrationError.notPostgres)
-				.flatMap { database in
-					database.query("""
-						ALTER TABLE \(Task.schema)
-						ALTER COLUMN sort_order SET NOT NULL
-						""")
-				}
-				.map { _ in }
+
+			var updatedAny = false
+			var sortOrder = 1
+			for task in try await database.query(SortableTask.self).all() {
+				updatedAny = true
+
+				task.sortOrder = sortOrder
+				sortOrder += 1
+				try await task.save(on: database)
+			}
+
+			guard updatedAny
+			else { return }
+
+			guard let database = database as? PostgresDatabase
+			else { throw MigrationError.notPostgres }
+
+			_ = try await database.query("""
+			ALTER TABLE \(Task.schema)
+			ALTER COLUMN sort_order SET NOT NULL
+			""").get()
 		}
 	}
 
-	func revert(on database: Database) -> EventLoopFuture<Void> {
-		return database.schema(TodoSettings.schema)
+	func revert(on database: Database) async throws {
+		try await database.schema(TodoSettings.schema)
 			.deleteField("sort_order")
 			.update()
 	}
